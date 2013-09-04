8ee505c54ec082fd888c926a7f4341160f35d092
Timothy Baldock <tb@entropy.me.uk>
2013-09-04 18:03:34 +0100

    Add interface for Players (implemented by SocketHandler and AI Players)

---
+++ test/Game.unit.tests.js
@@ -0,0 +1,17 @@
+var expect = require('expect.js');
+
+var Game = require('../lib/Game.js').Game;
+
+suite('Game', function () {
+    var sut;
+
+    setup(function () {
+        sut = new Game();
+    });
+
+    teardown(function () {
+    });
+
+    suite('contract', function () {
+    });
+});
